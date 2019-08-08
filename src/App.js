c8206926128435bea3c9a5a349d452a86420241a
Mostafa.drz <mostafa.drz@visioncritical.com>
2019-08-08 13:04:33 -0400

    add styled components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import styled from "styled-components";
 
 class App extends React.Component {
   state = {
@@ -20,6 +21,7 @@ class App extends React.Component {
       });
   };
   render() {
+    const { response } = this.state;
     return (
       <div className="App">
         <h1>Git Profile</h1>
@@ -31,9 +33,38 @@ class App extends React.Component {
         <button onClick={this.getUserInfo} className="btn btn-primary">
           Search
         </button>
+        {response ? (
+          <Container>
+            <ImageContainer src={response.avatar_url} alt={response.name} />
+            <h2>{response.name}</h2>
+            <p>{response.bio}</p>
+          </Container>
+        ) : null}
       </div>
     );
   }
 }
+
+const ImageContainer = styled.img`
+  width: 100px;
+  height: 100px;
+  border-radius: 100%;
+`;
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  margin: auto;
+  width: 700px;
+  h2 {
+    color: blue;
+    &:hover {
+      color: red;
+    }
+  }
+  p {
+    font-weight: 400;
+  }
+`;
 
 export default App;
